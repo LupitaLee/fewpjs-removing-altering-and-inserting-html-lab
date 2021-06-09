19954bc146ea3a23c065229efdd58e0e6f6bc5cc
Lupita <lupitarivera8899@gmail.com>
2021-06-09 15:09:20 -0500

    commit tree 78dff203a07d3b0c9f27d97868f1b47cc0f69fa4

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+document.body.appendChild(newHeader);// this willmake it  display in the browser 
